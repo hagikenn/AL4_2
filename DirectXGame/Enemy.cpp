d8c667a055f2e7a28714a8ebdaae9d60da22fd40
hagikenn <146914983+hagikenn@users.noreply.github.com>
2024-12-12 15:21:27 +0900

    01-07確認課題完了

--- DirectXGame/Enemy.cpp
+++ DirectXGame/Enemy.cpp
@@ -14,7 +14,27 @@ void Enemy::Update() {
 	// 座標移動（ベクトルの加算）
 	worldTransform_.translation_.z -= 0.1f;
 
+	// キャラクターの移動速さ
+	const Vector3 kCharacterSpeed = {0.05f, 0.05f, 0.05f};
+
 	worldTransform_.UpdateMatrix();
+
+	switch (phase_) {
+	case Enemy::Phase::Approach:
+	default:
+		//移動（ベクトルを加算）
+		worldTransform_.translation_ += kCharacterSpeed;
+		//規定の位置に到達したら離脱
+		if (worldTransform_.translation_.z < 0.0f) {
+			phase_ = Phase::Leave;
+		}
+		break;
+	case Enemy::Phase::Leave:
+		//移動（ベクトルを加算）
+		worldTransform_.translation_ += kCharacterSpeed;
+		break;
+	}
+
 }
 
 void Enemy::Draw(Camera& camera) {
--- DirectXGame/Enemy.h
+++ DirectXGame/Enemy.h
@@ -1,10 +1,16 @@
 #pragma once
 #include <KamataEngine.h>
 using namespace KamataEngine;
+#include"MathUtilityForText.h"
 
 
 class Enemy {
 public:
+	enum class Phase {
+		Approach,//接近する
+		Leave,//離脱する
+	};
+
 	void Initialize(Model* model, uint32_t textureHandle);
 	void Update();
 	void Draw(Camera& camera);
@@ -16,5 +22,7 @@ private:
 	Model* model_ = nullptr;
 	// テクスチャハンドル
 	uint32_t textureHandle_ = 0u;
+	//フェーズ
+	Phase phase_ = Phase::Approach;
 
 };
